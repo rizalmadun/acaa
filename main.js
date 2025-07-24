3197c60f55bc1b45ed76d84ec642d2aa5fce5592
rizalmadun <rizalmadun7@gmail.com>
2025-07-24 10:31:20 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function clicks() {
+    $('#div1').hide();
+    $('#div2').show();
+    $('#div2').addClass("fadeinv2");
+        
+    var typed = new Typed('.katakata', {
+        strings: ["Mass Cuma mau bilang", "good night acaaa", "Acaa tau engga mass tuh Sayang Banget Sama Acaa",  "makasih yaa acaa udah hadir di kehidupan mass","i love u acaaa selamat boboo yaa mimpi indah yaa cantikk" ],
+        typeSpeed: 70
+    });
+}
